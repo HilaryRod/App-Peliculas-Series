60d37529dbe5a3aa55a32f9b7d75e9d167e98edd
Hilary <hilaryvirginia49@gmail.com>
2025-08-25 18:27:52 -0600

    Ruta pantalla inicial login y para paginas no encontradas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Home from "./components/Home";
@@ -8,6 +8,8 @@ function App() {
   return (
     <Router>
       <Routes>
+         {/* Redirige la ruta raíz a Login */}
+        <Route path="/" element={<Navigate to="/login" />} />
         {/* Login */}
         <Route path="/login" element={<Login />} />
         {/* Register */}
@@ -16,8 +18,8 @@ function App() {
         <Route path="/home" element={<Home />} />
          {/* Lists */}
         <Route path="/lists" element={<Lists />} />
-
-
+        {/* Ruta para páginas no encontradas */}
+        <Route path="*" element={<h1>Página no encontrada</h1>} />
       </Routes>
     </Router>
   );
